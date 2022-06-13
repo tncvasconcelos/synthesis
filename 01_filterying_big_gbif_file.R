eb17b5c223cb59254c55a39d2c1ad15ca379290f
tncvasconcelos <thais.nogales@gmail.com>
2022-06-13 09:19:22 -0500

    filtering gbif

--- 01_filterying_big_gbif_file.R
+++ 01_filterying_big_gbif_file.R
@@ -36,9 +36,9 @@ fix.names.taxize <- function(focal_species_trees) {
 #-----------------------------
 # If local
 # setwd("~/Desktop/WCVP_special_issue/Eve_MyrtalesPAFTOL/myrtales")
-source("/Users/thaisvasconcelos/Desktop/WCVP_special_issue/WCVPtools/WCVPtools_functions.R")
-dist_sample <- read.table("/Users/thaisvasconcelos/Desktop/WCVP_special_issue/wcvp_names_and_distribution_special_edition_2022/wcvp_distribution.txt", sep="|", header=TRUE, quote = "", fill=TRUE, encoding = "UTF-8")
-names_sample <- read.table("/Users/thaisvasconcelos/Desktop/WCVP_special_issue/wcvp_names_and_distribution_special_edition_2022/wcvp_names.txt", sep="|", header=TRUE, quote = "", fill=TRUE, encoding = "UTF-8")
+#source("/Users/thaisvasconcelos/Desktop/WCVP_special_issue/WCVPtools/WCVPtools_functions.R")
+#dist_sample <- read.table("/Users/thaisvasconcelos/Desktop/WCVP_special_issue/wcvp_names_and_distribution_specia#l_edition_2022/wcvp_distribution.txt", sep="|", header=TRUE, quote = "", fill=TRUE, encoding = "UTF-8")
+#names_sample <- read.table("/Users/thaisvasconcelos/Desktop/WCVP_special_issue/wcvp_names_and_distribution_speci#al_edition_2022/wcvp_names.txt", sep="|", header=TRUE, quote = "", fill=TRUE, encoding = "UTF-8")
 #-----------------------------
 # If labcomputer
 # setwd("~/myrtales")
@@ -52,51 +52,57 @@ all_vars <- merge(dist_sample, names_sample, by="plant_name_id")
 # reference table for taxized names
 #-----------------------------
 # If local
-reference_table <- list.files("/Users/thaisvasconcelos/Desktop/WCVP_special_issue/WCVPtools/taxized_reference_tables", full.names = T)
-reference_table <- do.call(rbind, lapply(reference_table, read.csv))
-reference_table$gbif_name <- fix.names.taxize(reference_table$gbif_name)
+#reference_table <- list.files("/Users/thaisvasconcelos/Desktop/WCVP_special_issue/WCVPtools/taxized_reference_tables", full.names = T)
+#reference_table <- do.call(rbind, lapply(reference_table, read.csv))
+#reference_table$gbif_name <- fix.names.taxize(reference_table$gbif_name)
 
 #-----------------------------
 # If labcomputer
 reference_table <- list.files("../WCVPtools/taxized_reference_tables", full.names = T)
 reference_table <- do.call(rbind, lapply(reference_table, read.csv))
+reference_table$gbif_name <- fix.names.taxize(reference_table$gbif_name)
 
 # Reading gbif file
-gbif_data <- fread(file.choose()) # load the table you downloaded from GBIF
+gbif_data <- fread("gbif_big_files/0339018-210914110416597.csv") # load the table you downloaded from GBIF
 
 # Looking at the WCVP table and TDWG to clean GBIF points
 #-----------------------------
 # If local
-path="/Users/thaisvasconcelos/Desktop/WCVP_special_issue/WCVPtools/wgsrpd-master/level3/level3.shp"
+#path="/Users/thaisvasconcelos/Desktop/WCVP_special_issue/WCVPtools/wgsrpd-master/level3/level3.shp"
 #-----------------------------
 # If labcomputer
 path="../WCVPtools/wgsrpd-master/level3/level3.shp"
 #-----------------------------
 
 twgd_data <- suppressWarnings(maptools::readShapeSpatial(path))
-issues_to_remove <- read.csv("gbif_issues_to_remove.csv")
-cultivated <- read.csv("cultivated_species.csv")
-for(family_index in 1:length(myrtales_families)){
-  one_subset <- subset(gbif_data, gbif_data$family == myrtales_families[family_index]) #subsetting to make it more manageble
-  cleaned_points <- subset(one_subset, one_subset$basisOfRecord == "PRESERVED_SPECIMEN")
-  cleaned_points <- subset(cleaned_points, cleaned_points$scientificName!="")
+#issues_to_remove <- read.csv("gbif_issues_to_remove.csv")
+#cultivated <- read.csv("cultivated_species.csv")
+  cleaned_points <- subset(gbif_data, gbif_data$scientificName!="")
   cleaned_points <- FilterWCVP_genus(cleaned_points, all_vars, twgd_data)
-  for(issue_index in 1:nrow(issues_to_remove)) {
-    cleaned_points <- subset(cleaned_points, !grepl(issues_to_remove$issues_to_remove[issue_index], cleaned_points$issue))
-  }
-  for(cultivated_index in 1:nrow(cultivated)) {
-    cleaned_points <- subset(cleaned_points, !grepl(cultivated$cultivated_species[cultivated_index], cleaned_points$species))
-  }
+# for(issue_index in 1:nrow(issues_to_remove)) {
+#    cleaned_points <- subset(cleaned_points, !grepl(issues_to_remove$issues_to_remove[issue_index], cleaned_points$issue))
+#  }
+#  for(cultivated_index in 1:nrow(cultivated)) {
+#    cleaned_points <- subset(cleaned_points, !grepl(cultivated$cultivated_species[cultivated_index], cleaned_points$species))
+#  }
+  write.csv(cleaned_points, file="gbif_big_files/preliminary_cleaning1.csv")
+  
   subset_reference_table <- subset(reference_table, reference_table$gbif_name %in% unique(cleaned_points$scientificName))
   if(nrow(subset_reference_table)>0){
     cleaned_points <- FilterWCVP(cleaned_points, all_vars, subset_reference_table, twgd_data) # This will filter the GBIF points acording to WCVP for species
   }
+  write.csv(cleaned_points, file="gbif_big_files/preliminary_cleaning2.csv")
+  
   # Cleaning common problems:
-  cleaned_points <- RemoveNoDecimal(cleaned_points, lon="decimalLongitude", lat="decimalLatitude")
+  # cleaned_points <- RemoveNoDecimal(cleaned_points, lon="decimalLongitude", lat="decimalLatitude")
   cleaned_points <- RemoveCentroids(cleaned_points, lon="decimalLongitude", lat="decimalLatitude")
   cleaned_points <- RemoveDuplicates(cleaned_points, lon="decimalLongitude", lat="decimalLatitude")
-  cleaned_points <- RemoveOutliers(cleaned_points, species="scientificName", lon="decimalLongitude", lat="decimalLatitude")
-  cleaned_points <- RemoveZeros(cleaned_points, lon="decimalLongitude", lat="decimalLatitude")
-  cleaned_points <- RemoveSeaPoints(cleaned_points, lon="decimalLongitude", lat="decimalLatitude")
-  write.csv(cleaned_points, file=paste0("myrtales_gbif/", myrtales_families[family_index], "_cleaned_points.csv"), row.names=F)
-}
+  
+  write.csv(cleaned_points, file="gbif_big_files/preliminary_cleaning3.csv")
+  
+  
+  #cleaned_points <- RemoveOutliers(cleaned_points, species="scientificName", lon="decimalLongitude", lat="decimalLatitude")
+  #cleaned_points <- RemoveZeros(cleaned_points, lon="decimalLongitude", lat="decimalLatitude")
+  #cleaned_points <- RemoveSeaPoints(cleaned_points, lon="decimalLongitude", lat="decimalLatitude")
+  #write.csv(cleaned_points, file=paste0("myrtales_gbif/", myrtales_families[family_index], "_cleaned_points.csv"), row.names=F)
+#}
